39d5f1f94c3bb8f6bdbae3cab248c75f21da56c1
David Pickett <pickettd@gmail.com>
2015-11-22 09:05:24 -1000

    Add game scenes to build settings and use loadlevel to go to the game scene

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,6 +5,7 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log ("Level load requested " + name);
+		Application.LoadLevel(name);
 	}
 	public void QuitRequest() {
 		Debug.Log ("Quit requested");
