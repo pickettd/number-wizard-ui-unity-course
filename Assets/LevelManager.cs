a85901f883fb83d4451c887030e34107adce8e4d
David Pickett <pickettd@gmail.com>
2015-11-22 08:58:32 -1000

    Add logic in loadlevel to print requested level, wire up start button click to loadlevel with string input Game, add quit request function in levelmanager, and wire up quit button to quit function

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
-	void LoadLevel() {
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested " + name);
+	}
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
 	}
 }
