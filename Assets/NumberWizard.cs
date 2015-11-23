c82bd1ca820a797caa5e995b5e939b396092a35d
David Pickett <pickettd@gmail.com>
2015-11-23 08:20:15 -1000

    Import console-based number wizard logic, update for ui version, wire together with buttons, and add game instructions to complete game.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 10;
+
+	public Text text;
+
+	void Guess () {
+		guess = (max+min) / 2;
+		text.text = guess.ToString();
+		maxGuessesAllowed -= 1;
+		if (maxGuessesAllowed == 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		text.text = guess.ToString();
+		max += 1;
+
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		Guess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		Guess ();
+	}
+}
