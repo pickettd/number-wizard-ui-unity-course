084d89dcff3e35c9fdce5a85013fdd4df57d6674
David Pickett <pickettd@gmail.com>
2015-11-25 10:47:22 -1000

    Significantly increase difficulty for player to win. Add text to show guesses remaining.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -6,14 +6,17 @@ public class NumberWizard : MonoBehaviour {
 	int max;
 	int min;
 	int guess;
-	public int maxGuessesAllowed = 5;
+	public int maxGuessesAllowed = 9;
+	public Text maxGuessString;
 
 	public Text text;
 
 	void Guess () {
-		guess = Random.Range(min, max+1);
+		//guess = Random.Range(min, max+1);
+		guess = (max+min) / 2;
 		text.text = guess.ToString();
 		maxGuessesAllowed -= 1;
+		maxGuessString.text = maxGuessesAllowed.ToString();
 		if (maxGuessesAllowed == 0) {
 			Application.LoadLevel("Win");
 		}
@@ -23,9 +26,11 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
 		max = 1000;
 		min = 1;
-		guess = Random.Range(min, max+1);
+		//guess = Random.Range(min, max+1);
+		guess = 500;
 		text.text = guess.ToString();
-
+		maxGuessString.text = maxGuessesAllowed.ToString();
+		max += 1;
 	}
 
 	public void GuessHigher() {
